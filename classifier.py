0c170905171865bff2bd418e81cbccd67a609732
Emirhan <76557563+e3ayd@users.noreply.github.com>
2024-09-08 17:06:16 +0300

    Create classifier.py

---
+++ classifier.py
@@ -0,0 +1,7 @@
+# Define the categories and associated keywords
+CATEGORIES = {
+    "Work": ["project", "meeting", "deadline", "client"],
+    "Personal": ["birthday", "party", "family", "friend"],
+    "Promotions": ["sale", "discount", "offer", "promotion"],
+    "Spam": ["free", "win", "lottery", "prize"]
+}
